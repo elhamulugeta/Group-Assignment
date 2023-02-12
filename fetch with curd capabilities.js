42615effc020f4ca4475cd27d8308ff1954c63b1
UGR-8932-13 <120024349+UGR-8932-13@users.noreply.github.com>
2023-02-12 23:57:32 +0300

    Update fetch with curd capabilities.js

--- fetch with curd capabilities.js
+++ fetch with curd capabilities.js
@@ -1,17 +1,17 @@
-// this is a simple fetch api that has curd cabability in javascript
-fetch('https://jsonplaceholder.typicode.com/posts') //this will get the data from jsonplaceholder and store it into posts variable 
-    .then(response => response.json()) //convert the data to json format and store it into post variable 
-    .then(posts => {  //when we have the posts data, then we can do something with it like print or display on screen  
+
+fetch('https://jsonplaceholder.typicode.com/posts') 
+    .then(response => response.json())  
+    .then(posts => {    
 
         console.log(posts);
 
         let output = '';
 
-        for (let i = 0; i < posts.length; i++) { //for loop to iterate through all elements of array and add them one by one to output string 
+        for (let i = 0; i < posts.length; i++) {  
 
-            output += `<li>${posts[i].title}</li>`; //adding each element of array to output string using template literal syntax as shown below 
+            output += `<li>${posts[i].title}</li>`; 
 
         }
 
-        document.getElementById('output').innerHTML = output; //displaying the result inside id=output div tag on html page using innerHTML property of dom node object  
+        document.getElementById('output').innerHTML = output;   
     })
